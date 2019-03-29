15098cf1789bd2f8556d3de59ac45bf1dde560ab
Bec <a.becor94@gmail.com>
2019-03-29 12:32:56 -0600

    Complete login form

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -15,6 +15,7 @@ export class LoginComponent implements OnInit {
   submitted = false;
   cookieValue = 'UNKNOWN';
   random = '';
+  public formState: 'success' | 'fail' | 'blocked' | 'request' | 'clear' = 'clear';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -37,17 +38,25 @@ export class LoginComponent implements OnInit {
   }
 
   public onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      console.log(this.registerForm);
+      return;
+    }
 
     this.UserService.authorizeUser(this.registerForm.value.email, this.registerForm.value.password).subscribe((data) => {
-        console.log(data);
         // this.random = Math.random().toString(36).substr(2, 5);
-        this.cookieService.set('RL_16782_12', data.access_token);
-        this.router.navigate(['/profile'])
+        this.cookieService.set('RL_16782_12', data['access_token']);
+        this.router.navigate(['/profile']);
+
         // this.cookieService.set('honeg', this.random);
       }, (error) => {
 
         if (error.status === 401) {
-          alert('Credenciales invalidas');
+          this.formState = 'fail';
+          // alert('Credenciales invalidas');
         } else {
           console.error('Se ha encontrado el siguiente error, por favor contacte al administrador.');
           console.error(error);
